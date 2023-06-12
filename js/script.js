a1340bef9c62eff5c37f4e3f19750b917a25ee00
AKSHAY KUMAR TEKAM <46445922+akshaytekam1@users.noreply.github.com>
2023-06-12 17:31:23 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ===================== typing animation =========================*/
+
+var typed = new Typed(".typing", {
+    strings:["","Web Designer", "Web Developer", "Graphic Designer"],
+    typespeed:40,
+    BackSpeed:60,
+    loop:true
+})
